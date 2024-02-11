e0fd0c27d4ad4eede0e8018763a89313cd2d75a5
kutimiti1234 <tarbit.offensive@gmail.com>
2024-02-11 12:03:38 +0900

    memo表示機能の実装

--- app.rb
+++ app.rb
@@ -1,12 +1,15 @@
+# frozen_string_literal: true
+
 require 'sinatra'
 require 'sinatra/reloader'
+require 'csv'
 
 get '/' do
+  @title = ''
   erb :index
 end
 
-get '/reference' do
-  @title = 'タイトル'
-  @content = 'コンテント'
+get '/memos/*' do
+  @memo_id = params['splat'].first
   erb :index
 end
