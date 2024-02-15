920783a7694ceca6ab91cb87bd80cf3a90469211
kutimiti1234 <tarbit.offensive@gmail.com>
2024-02-15 23:25:58 +0900

    postでメモを追加する新規作成する機能を実装

--- app.rb
+++ app.rb
@@ -3,13 +3,24 @@
 require 'sinatra'
 require 'sinatra/reloader'
 require 'csv'
+require 'securerandom'
 
 get '/' do
   @title = ''
   erb :index
 end
 
-get '/memos/*' do
-  @memo_id = params['splat'].first
+post '/memos/' do
+  status 201
+  memos = CSV.read('./output/Sample.csv', headers: true)
+  new_id = SecureRandom.uuid
+  memos << CSV::Row.new(%w[id title content], [new_id, params['title'], params['content']])
+  File.write('./output/Sample.csv', memos)
+  erb :index
+end
+
+get '/memos/:memo_id' do
+  status 200
+  @memo_id = params['memo_id']
   erb :index
 end
