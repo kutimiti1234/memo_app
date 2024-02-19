24d48454a3b5536f5a99bc3b353ae69661b6ba03
kutimiti1234 <tarbit.offensive@gmail.com>
2024-02-19 23:20:57 +0900

    削除機能を実装。メモを0個にすることはできないように設定

--- app.rb
+++ app.rb
@@ -6,22 +6,51 @@ require 'csv'
 require 'securerandom'
 
 get '/' do
-  @memos = Memo.new
+  memos = Memo.new
+  @memos = memos
+  @show_memo_id = memos.getfirstmemoid
   erb :index
 end
 
 get '/memos/:memo_id' do
   status 200
-  @memos = Memo.new
-  @show_memo_id = params['memo_id']
+  memos = Memo.new
+  show_memo_id = params['memo_id']
+  pass unless memos.exist?(show_memo_id)
+
+  @memos = memos
+  @show_memo_id = show_memo_id
+
   erb :index
 end
 
 post '/memos/' do
   status 201
-  @memos = Memo.new
-  @memos.add(params[:title], params[:content])
+  memos = Memo.new
+  memos.add(title: params[:title], content: params[:content])
+  redirect '/'
   erb :index
+end
+
+delete '/memos/:memo_id' do
+  status 200
+  memos = Memo.new
+  show_memo_id = params['memo_id']
+  pass unless memos.exist?(show_memo_id)
+  pass if memos.length == 1
+
+  memos.delete(show_memo_id)
+  redirect '/'
+end
+
+delete '/memos/:memo_id' do
+  status 400
+  erb :alert
+end
+
+not_found do
+  status 404
+  '404 - Not Found'
 end
 
 # メモの読み書き、表示、削除用のクラス。
@@ -32,7 +61,7 @@ class Memo
     @memos = CSV.read('./output/Sample.csv', headers: true)
   end
 
-  def add(title, content)
+  def add(title:, content:)
     new_id = SecureRandom.uuid
     @memos << CSV::Row.new(%w[id title content], [new_id, title, content])
     File.write('./output/Sample.csv', @memos)
@@ -40,15 +69,28 @@ class Memo
 
   def update(id); end
 
-  def delete(id); end
+  def delete(id)
+    @memos = CSV::Table.new(@memos.reject { |row| row['id'] == id })
+    File.write('./output/Sample.csv', @memos)
+  end
 
   def showtitle(id)
-    id ||= @memos.first['id']
-    memos.select { |row| row['id'] == id }.first['title']
+    @memos.select { |row| row['id'] == id }.first['title']
   end
 
   def showcontent(id)
-    id ||= @memos.first['id']
-    memos.select { |row| row['id'] == id }.first['content']
+    @memos.select { |row| row['id'] == id }.first['content']
+  end
+
+  def getfirstmemoid
+    @memos.first['id']
+  end
+
+  def exist?(id)
+    !!@memos.select { |row| row['id'] == id }
+  end
+
+  def length
+    @memos.length
   end
 end
